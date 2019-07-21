8c5243015e7d951d1549b3eca238243cad65aad7
ZQHstudy <44081894+ZQHstudy@users.noreply.github.com>
2019-07-21 10:52:57 +0800

    ZQHStudy

---
+++ MyLibrary1/学习用的库1/MyLibrary/TestLibrart1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class TestLibrart1
+    {
+        public string Name { get; set; }
+        public string Pwd { get; set; }
+    }
+}
